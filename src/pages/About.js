87ad0386327b93fb18c804c8d94712ba4f99a6d6
HuydDo <huydinhdo@yahoo.com>
2022-02-06 16:02:32 -0700

    inital commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa quo architecto voluptatibus. Ducimus, aperiam. Rerum totam quasi officiis, blanditiis facere, soluta qui ducimus maxime neque aliquid quas, accusamus odio magni.
+      </p>
+    </section>
+  );
+};
+
+export default About;
